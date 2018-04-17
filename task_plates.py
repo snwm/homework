a5e49121bbd4674d3f8cf206ff8d68f5580027b9
Vitaly Ovchinnikov <snwm@ya.ru>
2018-04-18 00:16:01 +0300

    Задание №2

---
+++ task_plates.py
@@ -0,0 +1,17 @@
+﻿plate = int(input())
+detergent = int(input()) * 2
+
+limit = plate if plate < detergent else detergent
+
+i = 0
+while i < limit:
+	plate -= 1
+	detergent -= 1
+	i += 1
+
+if(plate > detergent):
+    print('Моющее средство закончилось. Осталось', plate, 'тарелок')
+elif(detergent > plate):
+    print('Все тарелки вымыты. Осталось', detergent / 2, 'ед. моющего средства')
+else:
+    print('Все тарелки вымыты, моющее средство закончилось')
