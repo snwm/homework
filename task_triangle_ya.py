4d04e220369bca741c6c86b9513c8dddd97751e5
Vitaly <snwm@ya.ru>
2018-04-18 10:12:02 +0300

    Задание №3

--- task_triangle_ya.py
+++ task_triangle_ya.py
@@ -2,11 +2,11 @@
 B = (int(input()), int(input()))
 C = (int(input()), int(input()))
 
-AB = ((A[0]-B[0])**2)+((A[1]-B[1])**2)
-AC = ((A[0]-C[0])**2)+((A[1]-C[1])**2)
-BC = ((B[0]-C[0])**2)+((B[1]-C[1])**2)
+AB = (A[0]-B[0])**2+(A[1]-B[1])**2
+AC = (A[0]-C[0])**2+(A[1]-C[1])**2
+BC = (B[0]-C[0])**2+(B[1]-C[1])**2
 
-if(AB == AC or AB == BC or AC == BC):
+if(AB == AC + BC or AC == BC + AB or BC == AC + AB):
     print("yes")
 else:
     print("no")
