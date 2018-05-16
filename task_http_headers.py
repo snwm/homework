9909bfb283aad4e01f95b43dd047de142c8a98fa
snwm <38378586+snwm@users.noreply.github.com>
2018-05-16 11:33:01 +0300

    Add files via upload
    Парсер HTTP-заголовков

---
+++ task_http_headers.py
@@ -0,0 +1,26 @@
+import json
+
+def http_headers_to_json(http, save_json):
+    dict = {}
+    get_answ = {
+	    "HTTP/": ['protocol', 'status_code', 'status_message'],
+		"GET": ['method', 'uri', 'protocol'],
+		"HTTP/2": ['protocol', 'status_code']
+	}
+    with open(http) as f:
+        for i, line in enumerate(f):
+            line = line.replace("\n","")
+            if(i == 0):
+                first_line = line.split()
+                answ = get_answ.get("GET") if not get_answ.get(first_line[0][:6]) else get_answ.get(first_line[0][:6])
+                if first_line[0] == "HTTP/2":
+                    answ = get_answ.get(first_line[0])
+                for i, head in enumerate(answ):
+                    dict[head] = first_line[i]
+            else:
+                if line:
+                    list = line.split(': ', maxsplit=1)
+                    dict[list[0]] = list[1]
+					
+    with open(save_json, 'w') as f:
+        json.dump(dict, f, indent=4)
