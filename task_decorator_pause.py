07fa0dc71fb3432008aac730f6c156ae4baac28a
Vitaly Ovchinnikov <snwm@ya.ru>
2018-05-20 17:41:01 +0300

    Декоратор pause

---
+++ task_decorator_pause.py
@@ -0,0 +1,11 @@
+﻿from functools import wraps
+import time
+
+def pause(t):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            time.sleep(t)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
