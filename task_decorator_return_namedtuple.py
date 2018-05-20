3742b290fdc23a0f50ffb759ab5ad4436f4eacda
Vitaly Ovchinnikov <snwm@ya.ru>
2018-05-20 20:58:25 +0300

    Декоратор return_namedtuple

---
+++ task_decorator_return_namedtuple.py
@@ -0,0 +1,15 @@
+﻿from functools import wraps
+from collections import namedtuple
+
+def return_namedtuple(*keys):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if isinstance(result, tuple):
+                Parts = {v:result[i] for i,v in enumerate(keys)}
+                name_tuple = namedtuple('Parts', Parts.keys())(**Parts)
+                return name_tuple
+        return wrapper
+    return decorator
+    
