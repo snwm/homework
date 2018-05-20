c938aaacb0cddc0c9562e2faaebba0fa021afb69
Vitaly Ovchinnikov <snwm@ya.ru>
2018-05-20 13:56:10 +0300

    Подправил

--- task_http_headers.py
+++ task_http_headers.py
@@ -11,16 +11,17 @@ def http_headers_to_json(http, save_json):
         for i, line in enumerate(f):
             line = line.replace("\n","")
             if(i == 0):
-                first_line = line.split()
-                answ = get_answ.get("GET") if not get_answ.get(first_line[0][:6]) else get_answ.get(first_line[0][:6])
+                first_line = line.split(" ", 2)
+                answ = get_answ.get("GET") if not get_answ.get(first_line[0][:5]) else get_answ.get(first_line[0][:5])
                 if first_line[0] == "HTTP/2":
                     answ = get_answ.get(first_line[0])
                 for i, head in enumerate(answ):
                     dict[head] = first_line[i]
             else:
                 if line:
-                    list = line.split(': ', maxsplit=1)
+                    list = line.split(': ', 1)
                     dict[list[0]] = list[1]
 					
     with open(save_json, 'w') as f:
         json.dump(dict, f, indent=4)
+        
